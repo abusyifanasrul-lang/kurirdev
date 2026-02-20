47366d00b3ab1b6e0f5a57e67c66ac4173f026ba
abusyifanasrul-lang <abusyifanasrul@gmail.com>
2026-02-20 20:34:47 +0800

    icon fix

--- vite.config.ts
+++ vite.config.ts
@@ -37,13 +37,25 @@ export default defineConfig({
             src: "icons/android/android-launchericon-192-192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
+          },
+          {
+            src: "icons/android/android-launchericon-192-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "icons/android/android-launchericon-512-512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
+          },
+          {
+            src: "icons/android/android-launchericon-512-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
           {
             src: "icons/ios/180.png",
